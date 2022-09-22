fbbd42e3061b4cd9098336daff63b3cff9680d34
ahmedelfran <78222735+ahmedelfran@users.noreply.github.com>
2022-09-22 15:31:05 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,111 @@
+let searchForm = document.querySelector('.search-form-container');
+let cart = document.querySelector('.shopping-cart-container');
+let loginForm = document.querySelector('.login-form-container');
+let navbar = document.querySelector('.header .navbar');
+
+// =============searchForm
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+    cart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+// ===============cart
+document.querySelector('#cart-btn').onclick = () => {
+    cart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+// ===============loginForm
+document.querySelector('#login-btn').onclick = () => {
+    loginForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cart.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+// ================navbar
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cart.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+
+
+
+
+// Swiper
+
+
+var swiper = new Swiper(".category-content", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    autoplay: {
+        delay: 15500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        450: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        550: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+        },
+        991: {
+            slidesPerView: 4,
+            spaceBetween: 15,
+        }
+    }
+});
+
+
+
+var swiper = new Swiper(".Review-box", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    gragCursor: 'true',
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        DynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+    }
+});
+
+
